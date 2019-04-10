0428e50c28f1e51b8bf5593dcf40c2bebda5b4fd
tranminhhieu90 <hieuminhtm90@gmail.com>
2019-04-11 00:01:16 +0700

    edir template login

--- resources/views/home-page/header.blade.php
+++ resources/views/home-page/header.blade.php
@@ -12,12 +12,6 @@
                     <span class="sr-only">(current)</span>
                 </a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Features</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Pricing</a>
-            </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown"
                    aria-haspopup="true" aria-expanded="false">Dropdown
