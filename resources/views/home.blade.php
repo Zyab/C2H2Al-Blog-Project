644c3e3c6337b867c38649b2bba4202566fae81c
tranminhhieu90 <hieuminhtm90@gmail.com>
2019-04-16 14:24:58 +0700

    fix conflict

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -36,38 +36,39 @@
                                                 <!-- Text -->
                                                 <p class="card-text">{{ $post->description }}</p>
                                                 <!-- Link -->
-                                                <a href="{{ route('blog.show', $post->id) }}" class="black-text d-flex justify-content-end"><h5>Read more
+                                                <a href="{{ route('blog.show', $post->id) }}"
+                                                   class="black-text d-flex justify-content-end"><h5>Read more
                                                         <i class="fas fa-angle-double-right"></i></h5></a>
 
                                             </div>
+                                        </div>
+                                        @endforeach
+                                        @endif
                                     </div>
-                                @endforeach
-                            @endif
-                                    </div>
-
-                    </div>
-                </div>
-                <div class="col-md-2" style="margin-bottom: 20px">
-                    <div class="card testimonial-card">
-                        <div class="card-up indigo lighten-1"></div>
-
-                        <!-- Avatar -->
-                        <div class="avatar mx-auto white">
-                            <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20%2810%29.jpg"
-                                 class="rounded-circle" alt="woman avatar"
-                                 style="height: 150px; width: 150px; margin-top: 20px">
                         </div>
-                        <!-- Content -->
-                        <div class="card-body">
-                            <!-- Name -->
-                            <h4 class="card-title">{{ Auth::user()->name }}</h4>
-                            <hr>
-                            <!-- Quotation -->
-                            <a class="btn peach-gradient btn-lg" href="{{route('blog.create')}}"
-                               style="background: #009688; color: whitesmoke"> Thêm mới bài viết</a>
-                            <hr>
-                            <a class="btn peach-gradient btn-lg" href="{{route('blog.list')}}"
-                               style="background: #009688; color: whitesmoke"> Danh sách bài viết</a>
+                    </div>
+                    <div class="col-md-2" style="margin-bottom: 20px">
+                        <div class="card testimonial-card">
+                            <div class="card-up indigo lighten-1"></div>
+
+                            <!-- Avatar -->
+                            <div class="avatar mx-auto white">
+                                <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20%2810%29.jpg"
+                                     class="rounded-circle" alt="woman avatar"
+                                     style="height: 150px; width: 150px; margin-top: 20px">
+                            </div>
+                            <!-- Content -->
+                            <div class="card-body">
+                                <!-- Name -->
+                                <h4 class="card-title">{{ Auth::user()->name }}</h4>
+                                <hr>
+                                <!-- Quotation -->
+                                <a class="btn peach-gradient btn-lg" href="{{route('blog.create')}}"
+                                   style="background: #009688; color: whitesmoke"> Thêm mới bài viết</a>
+                                <hr>
+                                <a class="btn peach-gradient btn-lg" href="{{route('blog.list')}}"
+                                   style="background: #009688; color: whitesmoke"> Danh sách bài viết</a>
+                            </div>
                         </div>
                     </div>
                 </div>
