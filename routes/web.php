30efd45f4e897a94a1e89962a0be064bffd534c8
tranminhhieu90 <hieuminhtm90@gmail.com>
2019-04-16 10:50:04 +0700

    ko cos gi

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-//Route::get('/{1}/edit', 'HomeController@index')->name('home');
+//Route::get('/{1}/edit', 'HomeController@index')->name('home')
