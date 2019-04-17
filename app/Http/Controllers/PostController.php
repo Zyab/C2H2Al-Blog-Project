f02fb29d53d207b86b27b9caa9b9f8a062d27eb3
phamlananh1 <phamlananh12011995@gmail.com>
2019-04-17 08:51:19 +0700

    create post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,6 +6,8 @@ use App\Post;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+use PhpOption\Tests\PhpOptionRepo;
 
 class PostController extends Controller
 {
@@ -13,10 +15,26 @@ class PostController extends Controller
         return view('blog.create');
     }
 
-    public function getAllPost(){
-        $user = User::all();
-        $posts = Auth::user()->posts;
-            return view('home', compact('posts','user'));
+//    public function getAllPost(){
+//        $user = User::all();
+//        $posts = Auth::user()->posts;
+//            return view('home', compact('posts','user'));
+//
+//    }
+    public function store(Request $request){
+        $post = new Post();
+        $post->title  = $request->input('title');
+        $post->content = $request->input('content');
+        $post->user_id = Auth::user()->id;
+        if ($request->hasFile('image')) {
+            $image = $request->image;
+            $path = $image->store('images', 'public');
+            $post->image = $path;
+        }
+        $post->save();
+        Session::flash('success', 'Tạo mới bài viết thành công');
+        return redirect()->route('blog.create');
 
     }
 }
+
