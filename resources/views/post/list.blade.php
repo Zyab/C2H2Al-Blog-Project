8d3c384f7f65eca7bde3ab35c0ecab1194d7104c
tranminhhieu90 <hieuminhtm90@gmail.com>
2019-04-18 21:26:37 +0700

    edit list balde

--- resources/views/post/list.blade.php
+++ resources/views/post/list.blade.php
@@ -13,9 +13,12 @@
                                         <div class="card">
                                             <!-- Card image -->
                                             <div class="view overlay">
-                                                <img class="card-img-top"
-                                                     src="{{asset('storage/'.$post->image)}}"
-                                                     alt="Card image cap" style="width: 100%; height: 500px">
+                                                <a href="{{ route('post.show', $post->id) }}">
+                                                    <img class="card-img-top"
+                                                         src="{{asset('storage/'.$post->image)}}"
+                                                         alt="Card image cap" style="width: 100%; height: 500px">
+                                                </a>
+
                                                 <a>
                                                     <div class="mask rgba-white-slight"></div>
                                                 </a>
@@ -25,17 +28,19 @@
                                             <div class="card-body">
 
                                                 <!-- Social shares button -->
-                                                <a class="activator waves-effect waves-light mr-4"><i
-                                                            class="fas fa-share-alt"></i></a>
+{{--                                                <a class="activator waves-effect waves-light mr-4"><i--}}
+{{--                                                            class="fas fa-share-alt"></i></a>--}}
                                                 <!-- Title -->
-                                                <b><h4 class="card-title">{{ $post->title }}</h4></b>
+                                                <a href="{{ route('post.show', $post->id) }}">
+                                                    <h4 class="card-title">{{ $post->title }}</h4></b>
+                                                </a>
                                                 <hr>
                                                 <!-- Text -->
                                                 <p class="card-text">{{ $post->description }}</p>
                                                 <!-- Link -->
-                                                <a href="{{ route('post.show', $post->id) }}"
-                                                   class="black-text d-flex justify-content-end"><h5>Read more
-                                                        <i class="fas fa-angle-double-right"></i></h5></a>
+{{--                                                <a href="{{ route('post.show', $post->id) }}"--}}
+{{--                                                   class="black-text d-flex justify-content-end"><h5>Read more--}}
+{{--                                                        <i class="fas fa-angle-double-right"></i></h5></a>--}}
                                             </div>
                                         </div>
                                     </div>
