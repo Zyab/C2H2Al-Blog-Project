9eed4982e97cf4a085b34e7955460fcd25073d6a
tranminhhieu90 <hieuminhtm90@gmail.com>
2019-04-19 14:57:45 +0700

    sua giao dien form login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-7">
+            <div class="col-md-8">
                 <div class="card">
                     @if(session('successMsg'))
                         <div class="alert alert-success" role="alert">
@@ -11,17 +11,18 @@
                         </div>
                     @endif
                     <h5 class="card-header info-color white-text text-center py-4"
-                        style="background: #009688!important ; color: white">
-                        <strong>{{ 'LOGIN' }}</strong>
+                        style="background: #2176bd!important ; color: white">
+                        <strong>{{ __('LOGIN') }}</strong>
                     </h5>
                     <div class="card-body px-lg-5 pt-0">
-                        <form method="POST" action="{{ route('login') }}" class="text-center ">
+                        <form method="POST" action="{{ route('login') }}" class="text-left ">
                             @csrf
+
                             <div class="md-form" style="padding-top: 15px">
-                                <h5 for="exampleInputEmail1"></h5>
+                                <label for="email">{{ __('EMAIL') }}</label>
                                 <input id="email" type="email"
                                        class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email"
-                                       value="{{ old('email') }}" placeholder="Enter Your Email" >
+                                       value="{{ old('email') }}" placeholder="Enter Your Email" required autofocus>
                                 <div class="col-md-6">
                                     @if ($errors->has('name'))
                                         <span class="invalid-feedback" role="alert">
@@ -32,7 +33,7 @@
                             </div>
 
                             <div class="md-form" style="padding-top: 15px">
-                                <h5 for="password">{{ __('PASSWORD') }}</h5>
+                                <label for="password">{{ __('PASSWORD') }}</label>
                                 <input id="password" type="password"
                                        class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
                                        name="password" placeholder="Enter Your Password"
@@ -67,23 +68,14 @@
                             <button class="btn btn-outline-info btn-rounded btn-block my-4 waves-effect z-depth-0"
                                     type="submit">LOGIN
                             </button>
-                            <h4>or sign in with:</h4>
-                            <a  class="btn-floating btn-fb btn-sm" style="color: #1d68a7; font-size: 50px" href="facebook.com" >
-                                <i class="fab fa-facebook"></i>
-                            </a>
-                            <a  class="btn-floating btn-tw btn-sm" style="color: #4bb1b1; font-size: 50px">
-                                <i class="fab fa-twitter"></i>
-                            </a>
-                            <a  class="btn-floating btn-li btn-sm" style="color: red; font-size: 50px">
-                                <i class="fab fa-google-plus-g"></i>
-                            </a>
-                            <a class="btn-floating btn-git btn-sm" style="color: #3D4852; font-size: 50px">
-                                <i class="fab fa-github"></i>
-                            </a>
+                            <button class="btn btn-outline-info btn-rounded btn-block my-4 waves-effect z-depth-0"
+                                    type="submit">LOGIN WITH FACEBOOK
+                            </button>
                         </form>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
 @endsection
