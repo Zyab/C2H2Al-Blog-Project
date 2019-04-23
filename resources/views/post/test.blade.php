7e2a269750dcdee6f01d466a63193acb77757b01
tranminhhieu90 <hieuminhtm90@gmail.com>
2019-04-23 15:08:05 +0700

    sua form register

--- resources/views/post/test.blade.php
+++ resources/views/post/test.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -12,7 +12,7 @@ Title: <p>{!! $posts->title !!}</p>
 <br>
 Content: <p>{!! $posts->content !!}</p>
 <br>
-Image: {{$posts->image}}
+{{--Image: <img src="{{asset("storage/$posts->image")}}" alt="" width="300px" height="300px">--}}
 </body>
 </html>
 
