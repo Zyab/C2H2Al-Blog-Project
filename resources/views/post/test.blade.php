97d5770718cf8863412b1a1da477a22c517917b5
tranminhhieu90 <hieuminhtm90@gmail.com>
2019-04-24 15:03:46 +0700

    sua chuc nang tim kiem

--- resources/views/post/test.blade.php
+++ resources/views/post/test.blade.php
@@ -6,6 +6,18 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+        @font-face {
+            font-family: cedarville-font-family;
+            src: url("{{ asset('fonts/Cedarville-Cursive.ttf') }}");
+            font-weight: normal;
+        }
+        @font-face {
+            font-family: cedarville-font-family;
+            src: url("{{ asset('fonts/Cadarville-Cursive.ttf') }}");
+            font-weight: bold;
+        }
+    </style>
 </head>
 <body>
 Title: <p>{!! $posts->title !!}</p>
